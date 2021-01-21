35464bf55ab69d018f5e42ac17230b4411572503
bishnunathbd <bishnunath774@gmail.com>
2021-01-21 20:07:22 +0600

    34-5-9-handle user phone number

--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusSquare } from '@fortawesome/free-solid-svg-icons';
+import { faPhoneSquare, faPlusSquare } from '@fortawesome/free-solid-svg-icons';
 import './User.css';
 
 const User = (props) => {
   const { name, email, phone, picture, salary } = props.user;
+
+  const [mobile, setMobile] = useState('');
+
+  const handleShowPhoneNo = () => setMobile(phone);
 
   return (
     <div className='user'>
@@ -13,9 +17,12 @@ const User = (props) => {
       </div>
       <div>
         <h2>{name.title + ' ' + name.first + ' ' + name.last}</h2>
-        <h4>Phone: {phone}</h4>
         <h4>Email: {email}</h4>
         <h4>Salary: {salary}</h4>
+        <h4>Phone: {mobile}</h4>
+        <button onClick={handleShowPhoneNo} className='add-btn'>
+          <FontAwesomeIcon icon={faPhoneSquare} /> Show Phone No.
+        </button>
         <button onClick={() => props.handleAddUser(props.user)} className='add-btn'>
           <FontAwesomeIcon icon={faPlusSquare} /> Add User
         </button>
